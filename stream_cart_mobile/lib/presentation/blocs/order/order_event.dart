a318b93f43a8fd848456fca4020bf08551dc51f7
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-08-05 00:58:08 +0700

    [Order] Bloc-State-Event

---
+++ stream_cart_mobile/lib/presentation/blocs/order/order_event.dart
@@ -0,0 +1,92 @@
+import 'package:equatable/equatable.dart';
+import '../../../domain/entities/order/create_order_request_entity.dart';
+
+abstract class OrderEvent extends Equatable {
+  const OrderEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class GetOrdersByAccountEvent extends OrderEvent {
+  final String accountId;
+  final int? pageIndex;
+  final int? pageSize;
+  final int? status;
+
+  const GetOrdersByAccountEvent({
+    required this.accountId,
+    this.pageIndex,
+    this.pageSize,
+    this.status,
+  });
+
+  @override
+  List<Object?> get props => [accountId, pageIndex, pageSize, status];
+}
+
+class GetOrderByIdEvent extends OrderEvent {
+  final String id;
+
+  const GetOrderByIdEvent({required this.id});
+
+  @override
+  List<Object?> get props => [id];
+}
+
+class GetOrderByCodeEvent extends OrderEvent {
+  final String code;
+
+  const GetOrderByCodeEvent({required this.code});
+
+  @override
+  List<Object?> get props => [code];
+}
+
+class CreateMultipleOrdersEvent extends OrderEvent {
+  final CreateOrderRequestEntity request;
+
+  const CreateMultipleOrdersEvent({required this.request});
+
+  @override
+  List<Object?> get props => [request];
+}
+
+class CancelOrderEvent extends OrderEvent {
+  final String id;
+
+  const CancelOrderEvent({required this.id});
+
+  @override
+  List<Object?> get props => [id];
+}
+
+class RefreshOrdersEvent extends OrderEvent {
+  final String accountId;
+  final int? status;
+
+  const RefreshOrdersEvent({
+    required this.accountId,
+    this.status,
+  });
+
+  @override
+  List<Object?> get props => [accountId, status];
+}
+
+class LoadMoreOrdersEvent extends OrderEvent {
+  final String accountId;
+  final int? status;
+
+  const LoadMoreOrdersEvent({
+    required this.accountId,
+    this.status,
+  });
+
+  @override
+  List<Object?> get props => [accountId, status];
+}
+
+class ResetOrderStateEvent extends OrderEvent {
+  const ResetOrderStateEvent();
+}
