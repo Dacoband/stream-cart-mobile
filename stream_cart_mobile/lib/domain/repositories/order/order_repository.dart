4bb0fff27a9ae328d2d6508b1956cdadd586a8a7
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-08-05 00:32:45 +0700

    [Order] : repository

---
+++ stream_cart_mobile/lib/domain/repositories/order/order_repository.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:stream_cart_mobile/core/error/failures.dart';
+import 'package:stream_cart_mobile/domain/entities/order/order_entity.dart';
+
+import '../../entities/order/create_order_request_entity.dart';
+
+abstract class OrderRepository {
+  // GET /api/orders/{id}
+  Future<Either<Failure, OrderEntity>> getOrderById(String id);
+  // POST /api/orders/multi
+  Future<Either<Failure, List<OrderEntity>>> createMultipleOrders(CreateOrderRequestEntity request);
+  // POST /api/orders/{id}/cancel
+  Future<Either<Failure, OrderEntity>> cancelOrder(String id);
+  // GET /api/orders/account/{accountId}
+  Future<Either<Failure, List<OrderEntity>>> getOrdersByAccountId(
+    String accountId, {
+    int? pageIndex,
+    int? pageSize,
+    int? status,
+  });
+  //Get order details by order code
+  Future<Either<Failure, OrderEntity>> getOrderDetailsByCode(String code);
+}
