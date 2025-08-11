59ea0c397e3c6280a8d32473a401c27d3646eb50
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-08-12 01:15:56 +0700

    [LiveKit]: Ice/turn config mobile using for it

--- stream_cart_mobile/lib/core/services/livekit_service.dart
+++ stream_cart_mobile/lib/core/services/livekit_service.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:livekit_client/livekit_client.dart';
 
 class LiveKitService {
@@ -26,12 +25,9 @@ class LiveKitService {
     final room = Room(roomOptions: roomOptions);
     _room = room;
     _listenRoomEvents();
-    debugPrint('[LiveKitService] Connecting url=$url relay=${forceRelay ? 'relay-only' : 'all'}');
     try {
       await room.connect(url, token, connectOptions: connectOptions);
-  debugPrint('[LiveKitService] Connected: localParticipant=${room.localParticipant?.identity}');
-    } catch (e, st) {
-      debugPrint('[LiveKitService] Connect error: $e\n$st');
+  } catch (e) {
       rethrow;
     }
   }
@@ -43,14 +39,8 @@ class LiveKitService {
     _listener = room.createListener();
     _listener!.on<RoomEvent>((event) {
       if (!_events.isClosed) _events.add(event);
-      if (kDebugMode) {
-        debugPrint('[LiveKitService] Event: ${event.runtimeType}');
-        if (event is RoomDisconnectedEvent) {
-          _lastDisconnect = event;
-          debugPrint('[LiveKitService] Disconnected');
-        } else if (event is TrackSubscribedEvent) {
-          debugPrint('[LiveKitService] TrackSubscribed kind=${event.publication.kind} sid=${event.publication.sid}');
-        }
+      if (event is RoomDisconnectedEvent) {
+        _lastDisconnect = event;
       }
       if (event is TrackPublishedEvent || event is ParticipantConnectedEvent) {
         forceSubscribeAll();
